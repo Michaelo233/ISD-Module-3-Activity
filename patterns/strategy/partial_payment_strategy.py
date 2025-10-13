ad771e830f858bcd5beaabf99c947b6129fd7708
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-10-13 12:58:50 -0500

    Third Commit:  Implemented abstract class process payment in the partial payment strategy class.

--- patterns/strategy/partial_payment_strategy.py
+++ patterns/strategy/partial_payment_strategy.py
@@ -2,3 +2,20 @@
 
 __author__ = ""
 __version__ = ""
+
+from patterns.strategy.payment_strategy import PaymentStrategy
+from billing_account.billing_account import BillingAccount
+
+class PartialPaymentStrategy(PaymentStrategy):
+
+    def process_payment(self, account, payee, amount):
+        BillingAccount.deduct_balance(payee, amount)
+        balance = BillingAccount.get_balance(payee, amount)
+        if balance <= 0:
+            message = (f"Processed payment of ${amount:,.2f}. "
+                       + f"New balance: ${balance:,.2f}.")
+        else:
+            message = (f"Partial payment of ${amount:,.2f} accepted. "
+                       + f"New balance: ${balance:,.2f}.")
+            
+        return message
