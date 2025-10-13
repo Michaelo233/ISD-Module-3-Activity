541ff55e3337c138685fc26fa7631cf16d1053a6
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-10-13 12:38:00 -0500

    Second Commit: Defined a super class for the payment strategy with abstract class.

--- patterns/strategy/payment_strategy.py
+++ patterns/strategy/payment_strategy.py
@@ -1,4 +1,15 @@
 """This module defines the PaymentStrategy class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Michael Obikwere"
+__version__ = "1.0.0"
+
+from billing_account.billing_account import BillingAccount
+from payee.payee import Payee
+from abc import ABC, abstractmethod
+
+class PaymentStrategy(ABC):
+
+    @abstractmethod
+    def process_payment(self, account: BillingAccount, payee:Payee, amount: float) ->str:
+        
+        pass
